653bee8450a9b0fdecfa385da0429fe8f35a8dcb
Masaki Miyata <catlover@38ta.com>
2020-09-04 15:15:56 +0900

    update: 名前の引き継ぎをhiddenからセッションに変更
    名前入力画面で入力された名前を年齢入力画面から年齢表示画面にセッションで引き継ぐ

--- src/main/java/penguin/web/controller/ProfileController.java
+++ src/main/java/penguin/web/controller/ProfileController.java
@@ -1,24 +1,33 @@
 package penguin.web.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.bind.support.SessionStatus;
 @Controller
 @RequestMapping("profile")
+@SessionAttributes(names = "name")
 public class ProfileController {
+    @ModelAttribute("name")
+    public String setName(String name) {
+        return name;
+    }
+
     @RequestMapping("name")
     public String name() {
         return "name";
     }
     @RequestMapping(path = "age", method = RequestMethod.POST)
     public String age(@RequestParam("name") String name, Model model) {
-        model.addAttribute("name", name);
+        setName(name);
         return "age";
     }
     @RequestMapping(path = "hello", method = RequestMethod.POST)
-    public String hello(@RequestParam("name") String name, @RequestParam("age") String age, Model model) {
-        model.addAttribute("name", name);
+    public String hello(@RequestParam("name") String name, @RequestParam("age") String age, Model model, SessionStatus sessionStatus) {
+        sessionStatus.setComplete();
         model.addAttribute("age", age);
         return "hello";
     }
